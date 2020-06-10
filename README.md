2e84943cfc4602024bae06c6a2003e7509ac47d9
OmarInCS <omarincs@gmail.com>
2020-06-10 10:09:45 +0300

    analysis bikeshare data

---
+++ README.md
@@ -0,0 +1,80 @@
+# Analysis of US Bikeshare Data
+### Course: Data Analysis Professional - Udacity
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+### Content:
+- bikeshare_2.py - code file
+- chicago.csv
+- new_york_city.csv
+- washington.csv
+
+### Tech:
+- Python
+- pandas
+- numpy
+
+### Resources:
+- Stackoverflow:
+    - [get-month-name-from-number](https://stackoverflow.com/questions/6557553/get-month-name-from-number)
+    - [Format numbers in f-string](https://stackoverflow.com/questions/45310254/fixed-digits-after-decimal-with-f-strings)
+- Python Documentation:
+    - [strftime-strptime-behavior](https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior) 
+
+
+### Sample Output:
+
+    Hello! Let's explore some US bikeshare data!
+
+    [+] Enter a city (chicago, new york city, washington): chicago
+    
+    [+] Enter a month (all, january, february, ... , june): january
+    
+    [+] Enter a day (all, Monday, Tuesday, ... , Sunday): Saturday
+    ----------------------------------------
+    
+    Calculating The Most Frequent Times of Travel...
+    
+    [+] Most common month: January
+    [+] Most common day of week: Saturday
+    [+] Most common start hour: 13
+    
+    This took 0.05425310134887695 seconds.
+    ----------------------------------------
+    
+    Calculating The Most Popular Stations and Trip...
+    
+    [+] Most commonly used start station: Streeter Dr & Grand Ave
+    [+] Most commonly used end station: Streeter Dr & Grand Ave
+    [+] Most frequent combination of start station and end station trip:
+            [*] Start: Streeter Dr & Grand Ave,
+            [*] End: Streeter Dr & Grand Ave
+    
+    This took 0.0068471431732177734 seconds.
+    ----------------------------------------
+    
+    Calculating Trip Duration...
+    
+    [+] Mean travel time: 16.0 min 0.50 sec
+    [+] Total travel time: 28 days 20:53:42
+    
+    This took 0.00033593177795410156 seconds.
+    ----------------------------------------
+    
+    Calculating User Stats...
+    
+    [+] Counts of user types:
+            [1] Subscriber          : 2123
+            [2] Customer            : 474
+    
+    [+] Counts of gender:
+            [1] Male                : 1585
+            [2] Female              : 542
+    
+    [+] Earliest year of birth:      1942.0
+    [+] Most recent year of birth:   2000.0
+    [+] Most common year of birth:   1989.0
+    
+    This took 0.0034639835357666016 seconds.
+    ----------------------------------------
+    
+    
+    Would you like to restart? Enter yes or no.
---
+++ bikeshare_2.py
@@ -0,0 +1,212 @@
+import time
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+months_list = ("All", "January", "February", "March", "April", "May", "June", "July", "August", 
+                   "September", "October", "November", "December")
+
+days_list = ("All", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    city = input("[+] Enter a city (chicago, new york city, washington): ").lower()
+    while city not in CITY_DATA:
+        print("[-] Invalid input, try again")
+        city = input("[+] Enter a city (chicago, new york city, washington): ").lower()
+
+    # get user input for month (all, january, february, ... , june)
+    
+    
+    month = input("[+] Enter a month (all, january, february, ... , june): ").capitalize()
+    while month not in months_list:
+        print("[-] Invalid input, try again")
+        month = input("[+] Enter a month (all, january, february, ... , june): ").capitalize()
+    
+
+    # get user input for day of week (all, monday, tuesday, ... sunday)
+    
+    day = input("[+] Enter a day (all, Monday, Tuesday, ... , Sunday): ").capitalize()
+    while day not in days_list:
+        print("[-] Invalid input, try again")
+        day = input("[+] Enter a day (all, Monday, Tuesday, ... , Sunday): ").capitalize()
+
+
+    print('-'*40)
+    return city, month, day
+
+
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+    
+    df = pd.read_csv(CITY_DATA[city])
+    df = df.iloc[:,1:] # remove first column
+    df["Start Time"] = df["Start Time"].apply(lambda st: datetime.strptime(st, "%Y-%m-%d %H:%M:%S"))
+    df["End Time"] = df["End Time"].apply(lambda st: datetime.strptime(st, "%Y-%m-%d %H:%M:%S"))
+    
+
+    
+    if month != "All":
+        month = months_list.index(month)
+        condition = df["Start Time"].apply(lambda st: st.month == month)
+        df = df[condition]
+        
+    if day != "All":
+        condition = df["Start Time"].apply(lambda st: st.strftime("%A") == day)
+        df = df[condition]
+     
+
+    return df
+
+
+def time_stats(df):
+    """Displays statistics on the most frequent times of travel."""
+
+    print('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+
+    # display the most common month
+    month = df["Start Time"].apply(lambda st: st.strftime("%B")).mode()[0]
+    print(f"[+] Most common month: {month}")
+
+
+    # display the most common day of week
+    day = df["Start Time"].apply(lambda st: st.strftime("%A")).mode()[0]
+    print(f"[+] Most common day of week: {day}")
+
+
+    # display the most common start hour
+    hour = df["Start Time"].apply(lambda st: st.strftime("%H")).mode()[0]
+    print(f"[+] Most common start hour: {hour}")
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def station_stats(df):
+    """Displays statistics on the most popular stations and trip."""
+
+    print('\nCalculating The Most Popular Stations and Trip...\n')
+    start_time = time.time()
+
+    # display most commonly used start station
+    station = df["Start Station"].mode()[0]
+    print(f"[+] Most commonly used start station: {station}")
+
+
+    # display most commonly used end station
+    station = df["End Station"].mode()[0]
+    print(f"[+] Most commonly used end station: {station}")
+
+
+    # display most frequent combination of start station and end station trip
+    path = df[["Start Station", "End Station"]].mode()
+    print(f"[+] Most frequent combination of start station and end station trip:")
+    print(f"\t[*] Start: {path['Start Station'][0]},\n\t[*] End: {path['End Station'][0]}")
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def trip_duration_stats(df):
+    """Displays statistics on the total and average trip duration."""
+
+    print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
+
+    # display total travel time
+    total_minutes, total_seconds = divmod(df["Trip Duration"].mean(), 60)
+    print(f"[+] Mean travel time: {total_minutes} min {total_seconds:.2f} sec")
+
+
+    # display mean travel time
+    total_minutes, total_seconds = divmod(df["Trip Duration"].sum(), 60)
+    total_hours, total_minutes = divmod(total_minutes, 60)
+    total_days, total_hours = divmod(total_hours, 24)
+    print(f"[+] Total travel time: {total_days} days {total_hours}:{total_minutes}:{total_seconds}")
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def user_stats(df):
+    """Displays statistics on bikeshare users."""
+
+    print('\nCalculating User Stats...\n')
+    start_time = time.time()
+
+    # Display counts of user types
+    types_count = df["User Type"].value_counts()
+    print("[+] Counts of user types:")
+    for i, count in enumerate(types_count):
+        print(f"\t[{i+1}] {types_count.index[i]:20s}: {count}")
+
+
+    # Display counts of gender
+    if "Gender" in df.columns:
+        gender_count = df["Gender"].value_counts()
+        print("\n[+] Counts of gender:")
+        for i, count in enumerate(gender_count):
+            print(f"\t[{i+1}] {gender_count.index[i]:20s}: {count}")
+
+
+
+    # Display earliest, most recent, and most common year of birth
+    if "Birth Year" in df.columns:
+        earliest_year = df["Birth Year"].min()
+        recent_year = df["Birth Year"].max()
+        common_year = df["Birth Year"].mode()[0]
+        
+        print("\n[+] Earliest year of birth:\t", int(earliest_year))
+        print("[+] Most recent year of birth:\t", int(recent_year))
+        print("[+] Most common year of birth:\t", int(common_year))
+
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
